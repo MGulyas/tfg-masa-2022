190f76f76d9f2b8477332effc72d11a2d8ded335
masa <masa.gulyas@wallbox.com>
2022-09-20 12:29:42 +0200

    implemented depth and normal integrators

--- src/integrators/depth_integrator.py
+++ src/integrators/depth_integrator.py
@@ -1,12 +1,16 @@
+from src.common.color import BLACK, RGBColor
 from src.integrators.integrator import Integrator
 
 
 class DepthIntegrator(Integrator):
 
-    def __init__(self, filename_, max_depth_=10):
+    def __init__(self, filename_, max_depth_=5):
         super().__init__(filename_ + '_Depth')
         self.max_depth = max_depth_
 
-    def compute_color(self, ray):
-        # ASSIGNMENT 1.3: PUT YOUR CODE HERE
-        pass
+    def compute_color(self, ray, scene):
+        hit_data = scene.closest_hit(ray)
+        if hit_data.has_hit:
+            c = max(1-hit_data.hit_distance/self.max_depth, 0)
+            return RGBColor(c, c, c)
+        return BLACK
--- src/integrators/normal_integrator.py
+++ src/integrators/normal_integrator.py
@@ -1,3 +1,5 @@
+from src.common.color import RGBColor, BLACK
+from src.common.vector_3d import Vector3D
 from src.integrators.integrator import Integrator
 
 
@@ -6,6 +8,9 @@ class NormalIntegrator(Integrator):
     def __init__(self, filename_):
         super().__init__(filename_ + '_Normal')
 
-    def compute_color(self, ray):
-        # ASSIGNMENT 1.3: PUT YOUR CODE HERE
-        pass
+    def compute_color(self, ray, scene):
+        hit_data = scene.closest_hit(ray)
+        if hit_data.has_hit:
+            color = (hit_data.normal + Vector3D(1, 1, 1))/2
+            return RGBColor(color.x, color.y, color.z)
+        return BLACK
